33fbb229d14ed3a28432885ce46b216e789e6449
Francisco wandallis pereira de sousa <100495942+Wandallis@users.noreply.github.com>
2023-05-08 19:36:02 -0300

    questao2.py

---
+++ questao2.py
@@ -0,0 +1,8 @@
+#2. Faça um programa que leia um inteiro n ≥ 0. Escreva n vezes a seguinte mensagem: Estou sabendo
+
+
+n = int(input("digite um inteiro maior ou igual a zero:"))
+while(n > 0):
+    n -= 1
+    print("Estou sabendo")
+
