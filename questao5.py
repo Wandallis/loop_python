faf0b11313ef04cc0b23002045b67c8e876eb9c2
Francisco wandallis pereira de sousa <100495942+Wandallis@users.noreply.github.com>
2023-05-08 19:40:17 -0300

    questao5.py

---
+++ questao5.py
@@ -0,0 +1,8 @@
+#5. Escreva um programa que declare um inteiro, inicialize-o com 0, e incremente-o de 1000 em 1000,imprimindo seu valor na tela, até que seu valor seja 100000 (cem mil).
+
+n = 0 
+while(True):
+    n += 1000
+    print(n)
+    if n == 100000:
+       break
